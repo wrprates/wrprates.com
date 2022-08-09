c521545c0697bae6eb9245d2cbb0da354d0fe72e
Wlademir Prates <wrprates@yahoo.com>
2022-08-09 01:57:10 -0300

    starting post about unicorns dataset

--- main.R
+++ main.R
@@ -2,7 +2,10 @@ library(quarto)
 
 # Copy and paste Disqus HTML to all posts
 posts_folders <- list.dirs(path = "posts", recursive = FALSE)
-file.copy("disqus.html", posts_folders, overwrite = TRUE)
+
+lapply(posts_folders, function(folder) {
+  file.copy("disqus.html", folder, overwrite = TRUE)
+})
 
 # Render website
 quarto::quarto_render()
